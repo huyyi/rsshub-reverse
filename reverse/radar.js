f23e692b72daf3949bceb59447e53d852422d514
jinkin <nadph513@outlook.com>
2022-03-25 14:28:58 +0800

    set-okteto

---
+++ reverse/radar.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'reverse': {
+        _name: '反向代理RSS',
+        '.': [
+            {
+                title: 'reverse_proxy',
+                docs: 'https://docs.rsshub.app/new-media.html#xin-hua-wang-xin-hua-she-xin-wen',
+                source: ['/'],
+                target: '/news/whxw',
+            },
+        ],
+    },
+};
---
+++ reverse/reverse.js
@@ -0,0 +1,21 @@
+const parser = require('@/utils/rss-parser');
+
+console.log("router ok");
+module.exports = async (ctx) => {
+    const { url } = ctx.params;
+    const feed = await parser.parseURL(url);
+
+    ctx.state.data = {
+        title: feed.title,
+        link: feed.link,
+        description: feed.description,
+        allowEmpty: false,
+        item: feed.items.map((item) => ({
+            title: item.title,
+            description: item.content,
+            pubDate: item.pubDate,
+            link: item.link,
+            author: item.creator,
+        })),
+    };
+};
